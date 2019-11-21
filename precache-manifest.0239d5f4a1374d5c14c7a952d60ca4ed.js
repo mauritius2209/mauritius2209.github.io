dc7d42998e5e2b11a7d33511c91b9e9b97ca5b8e
mauritius2209 <mauritius29lase@gmail.com>
2019-11-21 17:49:59 +0700

    UGD12_PWA_C_9413

---
+++ precache-manifest.0239d5f4a1374d5c14c7a952d60ca4ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d676526e0f50a40f1f9b",
+    "url": "/css/about.edc763d1.css"
+  },
+  {
+    "revision": "ffb4749f55cd48bc24a0",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "b14aadcd6552773c36c5b17202588f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d676526e0f50a40f1f9b",
+    "url": "/js/about.13593a51.js"
+  },
+  {
+    "revision": "10f011cc7d0dea80f1dd",
+    "url": "/js/app.44aacd75.js"
+  },
+  {
+    "revision": "ffb4749f55cd48bc24a0",
+    "url": "/js/chunk-vendors.2ac3a868.js"
+  },
+  {
+    "revision": "46fd9d7e152ff84bf67ab049174f124e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
